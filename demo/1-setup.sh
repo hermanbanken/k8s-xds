44ae1d8fab1bf0899b132ebc9d854c29b18efbe4
Herman <herman@q42.nl>
2022-05-18 22:09:15 +0200

    Kustomize bases

--- demo/1-setup.sh
+++ demo/1-setup.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 brew install kind
 
 ## For M1 Apple Silicon, continue below by creating a local registry (https://github.com/admiraltyio/admiralty/issues/148)
@@ -47,28 +48,29 @@ data:
     help: "https://kind.sigs.k8s.io/docs/user/local-registry/"
 EOF
 
-if false do
+kubectl create configmap jaeger -o yaml --dry-run=client | kubectl apply -f -
+if false
+then
   # Jaeger (https://www.jaegertracing.io/docs/1.33/deployment/#all-in-one)
   kubectl create deployment jaeger --image="jaegertracing/all-in-one:1.33" --replicas=1 --port=14268
   kubectl set env deployment/jaeger COLLECTOR_ZIPKIN_HOST_PORT=:9411
   kubectl expose deployment jaeger --port=14268 --target-port=14268 --selector='app=jaeger' # tracing ingress
-  cat <<EOF | kubectl apply -f -
-  apiVersion: v1
-  kind: Service
-  metadata:
-    name: jaeger-ui
-  spec:
-    type: NodePort
-    ports:
-    - name: http
-      nodePort: 32686
-      port: 16686
-    selector:
-      app: jaeger
-EOF
   kubectl create configmap jaeger --from-literal=JAEGER_TRACE_URL=http://jaeger:14268/api/traces -o yaml --dry-run=client | kubectl apply -f -
-else
-  kubectl create configmap jaeger -o yaml --dry-run=client | kubectl apply -f -
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: Service
+metadata:
+  name: jaeger-ui
+spec:
+  type: NodePort
+  ports:
+  - name: http
+    nodePort: 32686
+    port: 16686
+  selector:
+    app: jaeger
+EOF
+
 fi
 
 # Locally:
